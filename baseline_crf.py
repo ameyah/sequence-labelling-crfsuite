ea4b8a29d8d1fc9ab9ad66b1f2771b6cf26611ac
Ameya <ameyah@live.com>
2016-11-23 02:08:52 -0800

    Fixed CRFsuite tagging parameters. Code cleaned - Reused code

--- baseline_crf.py
+++ baseline_crf.py
@@ -1,3 +1,4 @@
+import glob
 from hw3_corpus_tool import get_utterances_from_filename, get_data
 import argparse
 import os
@@ -23,41 +24,32 @@ class BaselineCrf():
                     utterances_list = get_utterances_from_filename(os.path.join(input_dir, file_name))
                     # self.raw_data[]
 
+        @staticmethod
+        def get_features_act_tags(dialogue):
+            features = []
+            act_tags = []
+            first_utterance = True
+            current_speaker = dialogue[0].speaker
+            for utterance in dialogue:
+                utterance_feature = []
+                if first_utterance:
+                    utterance_feature.append("FEATURE_FU")
+                    first_utterance = False
+                elif current_speaker != utterance.speaker:
+                    utterance_feature.append("FEATURE_SC")
+                    current_speaker = utterance.speaker
+                if utterance.pos:
+                    utterance_feature.extend(["TOKEN_" + word.token for word in utterance.pos])
+                    utterance_feature.extend(["POS_" + word.pos for word in utterance.pos])
+                features.append(utterance_feature)
+                act_tags.append(utterance.act_tag)
+            return features, act_tags
+
         def scan_input_dir(self, input_dir):
-            for file_name in os.listdir(input_dir):
-                if file_name.endswith(".csv"):
-                    single_file_features = []
-                    single_file_act_tags = []
-                    utterances_list = get_utterances_from_filename(os.path.join(input_dir, file_name))
-                    current_speaker = utterances_list[0].speaker
-                    first_utterance = True
-                    for utterance in utterances_list:
-                        token_list = []
-                        pos_tag_list = []
-                        utterance_features = []
-                        try:
-                            for word_tag in utterance.pos:
-                                token_list.append("TOKEN_" + str(word_tag.token))
-                                if word_tag.pos:
-                                    pos_tag_list.append("POS_" + str(word_tag.pos))
-                        except TypeError as e:
-                            pass
-                        if first_utterance:
-                            # utterance_features.extend(['1', '0'])
-                            utterance_features.extend(['FEATURE_FU'])
-                            first_utterance = False
-                        elif current_speaker != utterance.speaker:
-                            # utterance_features.extend(['0', '1'])
-                            utterance_features.extend(['FEATURE_SC'])
-                            current_speaker = utterance.speaker
-                        utterance_features.extend(token_list)
-                        utterance_features.extend(pos_tag_list)
-                        single_file_features.append(utterance_features)
-                        single_file_act_tags.append(utterance.act_tag)
-                    # self.trainer.append(single_file_features, single_file_act_tags)
-                    self.features.extend(single_file_features)
-                    self.act_tags.extend(single_file_act_tags)
-            self.trainer.append(self.features, self.act_tags)
+            train_data = get_data(input_dir)
+            for dialogue in train_data:
+                features, act_tags = self.get_features_act_tags(dialogue[1])
+                self.trainer.append(features, act_tags)
 
         def train_model(self):
             # self.trainer.set_params({'c1': 1.0, 'c2': 0.80, 'max_iterations': 43, 'feature.possible_transitions': True})
@@ -66,34 +58,11 @@ class BaselineCrf():
 
         def tag_dir(self, test_dir):
             self.tagger.open('sequence_label_model.crfsuite')
-            for file_name in os.listdir(test_dir):
-                if file_name.endswith(".csv"):
-                    utterances_list = get_utterances_from_filename(os.path.join(test_dir, file_name))
-                    current_speaker = utterances_list[0].speaker
-                    first_utterance = True
-                    for utterance in utterances_list:
-                        token_list = []
-                        pos_tag_list = []
-                        utterance_features = []
-                        try:
-                            for word_tag in utterance.pos:
-                                token_list.append("TOKEN_" + str(word_tag.token))
-                                if word_tag.pos:
-                                    pos_tag_list.append("POS_" + str(word_tag.pos))
-                        except TypeError as e:
-                            pass
-                        if first_utterance:
-                            # utterance_features.extend(['1', '0'])
-                            utterance_features.extend(['FEATURE_FU'])
-                            first_utterance = False
-                        elif current_speaker != utterance.speaker:
-                            # utterance_features.extend(['0', '1'])
-                            utterance_features.extend(['FEATURE_SC'])
-                            current_speaker = utterance.speaker
-                        utterance_features.extend(token_list)
-                        utterance_features.extend(pos_tag_list)
-                        utterance_tag = self.tagger.tag([utterance_features])
-                        self.tag_data[file_name].append(utterance_tag)
+            test_data = get_data(test_dir)
+            for dialogue in test_data:
+                utterances = dialogue[1]
+                features, act_tags = self.get_features_act_tags(utterances)
+                self.tag_data[dialogue[0]].extend(self.tagger.tag(features))
 
         def write_data(self, write_file):
             # print(self.tag_data)
@@ -101,9 +70,9 @@ class BaselineCrf():
                 with open(write_file, "w", encoding='latin1') as file_handler:
                     for file_name in self.tag_data:
                         try:
-                            file_handler.write("Filename=\"" + str(file_name) + "\"" + '\n')
+                            file_handler.write("Filename=\"" + str(os.path.basename(file_name)) + "\"" + '\n')
                             for tag in self.tag_data[file_name]:
-                                file_handler.write(str(tag[0]) + '\n')
+                                file_handler.write(str(tag) + '\n')
                             file_handler.write('\n')
                         except:
                             continue
